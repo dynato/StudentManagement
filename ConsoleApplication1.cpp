8b49fb3406b418e47ab21747044059cd2f6e27f1
DESKTOP-EU71GTH\Dynato <dynato@gmail.com>
2020-11-03 20:47:37 +0700

    feat: Class List

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+
+class List
+{
+public:
+	List() {
+
+	}
+	virtual void insert() = 0;
+	virtual void eject() = 0;
+};
+
+class Stack
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
